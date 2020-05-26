61f8d4e1e7c49adfb5803f738ccbb8498a1116bb
39roc <funkidz39@gmail.com>
2020-05-26 12:08:52 +0900

    [ 프로젝트 생성 및 패키지 구조 구성]

---
+++ src/main/java/com/project/bit/BitApplication.java
@@ -0,0 +1,13 @@
+package com.project.bit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/bit/BitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
