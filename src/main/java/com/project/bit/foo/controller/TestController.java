2d3fd7e89785779c60c38d5c703ccde5707ed617
ваше имяRoman <dragon-rain@mail.ru>
2020-06-30 14:02:09 +0900

    Roman 2020.06.30

--- src/main/java/com/project/bit/foo/controller/TestController.java
+++ src/main/java/com/project/bit/foo/controller/TestController.java
@@ -1,22 +1,17 @@
 package com.project.bit.foo.controller;
 
 import java.security.Principal;
-import java.util.List;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.project.bit.foo.domain.Duty;
 import com.project.bit.foo.domain.Positions;
 import com.project.bit.foo.domain.ProjectMembers;
 import com.project.bit.foo.domain.Teams;
 import com.project.bit.foo.domain.Users;
-import com.project.bit.foo.domain.event.Event;
 import com.project.bit.foo.mapper.EventMapper;
-import com.project.bit.foo.service.DutyServiceImpl;
 import com.project.bit.foo.service.PositionsServiceImpl;
 import com.project.bit.foo.service.ProjectMembersServiceImpl;
 import com.project.bit.foo.service.TeamsServiceImpl;
@@ -31,7 +26,6 @@ import lombok.extern.slf4j.Slf4j;
 public class TestController {
 
 	private UserServiceImpl userServiceImpl;
-	private DutyServiceImpl dutyServiceImpl;
 	private ProjectMembersServiceImpl projectMembersServiceImpl;
 	private PositionsServiceImpl positionsServiceImpl;
 	private TeamsServiceImpl teamsServiceImpl;
@@ -60,7 +54,6 @@ public class TestController {
 
 	@PostMapping("/registration")
 	public String registrationPost(Users user) {
-		
 		System.out.println(user);
 		user.setUserPw(bCryptPasswordEncoder.encode(user.getUserPw()));
 		userServiceImpl.insertUser(user);
@@ -80,16 +73,6 @@ public class TestController {
 	@GetMapping("/addPositions")
 	public String addPositions() {
 		return "addPositions";
-	}
-	
-	@GetMapping("/usersList")
-	public String listPost(Model model) {
-		List<Event> events = eventMapper.selectEventById("333");
-		List<Users> users = userServiceImpl.selectAll();
-		model.addAttribute("users", users);
-		model.addAttribute("events", events);
-		System.out.println(users);
-		return "list";
 	}
 
 	@GetMapping("/addTeams")
@@ -111,12 +94,6 @@ public class TestController {
 	public String addMembersPost(ProjectMembers ProjectMembers) {
 		projectMembersServiceImpl.insertProjectMembers(ProjectMembers);
 		return "addMembers";
-	}
-
-	@PostMapping("/addDuty")
-	public String addDutyPost(Duty duty) {
-		dutyServiceImpl.insertDuty(duty.getDUTY_CODE(), duty.getDUTY_NAME());
-		return "addDuty";
 	}
 
 	@PostMapping("/addPositions")
